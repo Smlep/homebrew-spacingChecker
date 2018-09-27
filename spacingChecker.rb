8757a494050b17e8850bd50c82d06ac1550647ed
Sam Courtemanche <sam.courtemanche@epita.fr>
2018-09-27 20:41:01 +0200

    Added homebrew ruby script

---
+++ spacingChecker.rb
@@ -0,0 +1,15 @@
+class SpacingChecker < Formula
+  desc "A shell tool to check spacing around punctuation"
+  homepage "https://github.com/Smlep/SpacingChecker"
+  url "https://raw.githubusercontent.com/Smlep/install/master/install.sh"
+  sha256 "3973afab287cd7852051ecd757950d737ff56adf329be3f9faf575ea0b0e1e85"
+  version "1.0.0"
+
+  depends_on "grep"
+
+  bottle :unneeded
+
+  def install
+    sudo ./install.sh
+  end
+end
