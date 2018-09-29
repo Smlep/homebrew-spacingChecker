7cc74022ad8b18047c9a7dad5c393105b0bae1f3
Smlep <sam.courtemanche@epita.fr>
2018-09-29 18:22:22 +0200

    Update spacingChecker.rb

--- spacingChecker.rb
+++ spacingChecker.rb
@@ -2,7 +2,7 @@ class Spacingchecker < Formula
   desc "Shell tool to check spacing around punctuation"
   homepage "https://github.com/Smlep/SpacingChecker"
   url "https://raw.githubusercontent.com/Smlep/SpacingChecker/master/spacingChecker.tar.gz"
-  version "1.0.1"
+  version "1.0.2"
   sha256 "e3999aaef06b950e4879d41b8dda351f6a166c8f52cea69ba4db50994d58cad8"
 
   bottle :unneeded
