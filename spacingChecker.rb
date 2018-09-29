1d9c3dd3afa01ccf01dbee6c87dfb41957512c41
Smlep <sam.courtemanche@epita.fr>
2018-09-29 11:51:38 +0200

    Update spacingChecker.rb

--- spacingChecker.rb
+++ spacingChecker.rb
@@ -1,13 +1,13 @@
 class Spacingchecker < Formula
-  desc "A shell tool to check spacing around punctuation"
+  desc "Shell tool to check spacing around punctuation"
   homepage "https://github.com/Smlep/SpacingChecker"
   url "https://raw.githubusercontent.com/Smlep/SpacingChecker/master/spacingChecker.tar.gz"
-  sha256 "e3999aaef06b950e4879d41b8dda351f6a166c8f52cea69ba4db50994d58cad8"
   version "1.0.1"
-
-  depends_on "grep"
+  sha256 "e3999aaef06b950e4879d41b8dda351f6a166c8f52cea69ba4db50994d58cad8"
 
   bottle :unneeded
+  
+  depends_on "grep"
 
   def install
     libexec.install "languages/"
